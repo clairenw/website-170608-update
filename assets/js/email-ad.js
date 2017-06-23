91afb9d1071b210a4a755c3816bc523a5a834776
Steve Denning <steve.denning@heathwallace.com>
2017-06-23 10:22:30 +0100

    Footer amends
    Changed layout to footer on all pages

---
+++ assets/js/email-ad.js
@@ -0,0 +1,14 @@
+//<![CDATA[
+<!--
+var x="function f(x){var i,o=\"\",ol=x.length,l=ol;while(x.charCodeAt(l/13)!" +
+"=52){try{x+=x;l+=l;}catch(e){}}for(i=l-1;i>=0;i--){o+=x.charAt(i);}return o" +
+".substr(0,ol);}f(\")58,\\\"t\\\\100\\\\310\\\\600\\\\410\\\\320\\\\M400\\\\" +
+"620\\\\ZFHOQGFPHd?jo]%8Ekyy|`pwcyK.y~J(U5bjqmw\\\"\\\\#vPZNSTJF200\\\\DTvm7" +
+"71\\\\yL'm{r|wa`-ze!an\\\"\\\\xdfase`vjfur730\\\\=320\\\\720\\\\620\\\\430\\"+
+"\\020\\\\M130\\\\100\\\\030\\\\230\\\\320\\\\430\\\\R3S310\\\\t\\\\130\\\\2" +
+"00\\\\It\\\\[DMn\\\\710\\\\700\\\\520\\\\t\\\\-)s(5?4-491\\\"(f};o nruter};" +
+"))++y(^)i(tAedoCrahc.x(edoCrahCmorf.gnirtS=+o;721=%y;i=+y)58==i(fi{)++i;l<i" +
+";0=i(rof;htgnel.x=l,\\\"\\\"=o,i rav{)y,x(f noitcnuf\")"                     ;
+while(x=eval(x));
+//-->
+//]]>
